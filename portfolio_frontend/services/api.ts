cbf504b4fc6130fb6a0521e2319211fdf2195859
maxx_PC <m.petrykin@gmx.de>
2025-09-07 21:30:51 +0200

    front + back launched

--- portfolio_frontend/services/api.ts
+++
@@ -1,7 +0,0 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
-
-export async function fetchSomething() {
-  const res = await fetch(`${API_URL}/api/some-endpoint/`);
-  if (!res.ok) throw new Error("API error");
-  return res.json();
-}

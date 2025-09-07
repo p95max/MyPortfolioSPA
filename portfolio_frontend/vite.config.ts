56cb7a408479cc9add5ef1530b8afb48b44cecdf
maxx_PC <m.petrykin@gmx.de>
2025-09-07 21:05:46 +0200

    front + back launched

--- portfolio_frontend/vite.config.ts
+++ portfolio_frontend/vite.config.ts
@@ -1,23 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    host: true,
     port: 5173,
     proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/media": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+      '/api': 'http://web:8000',
+    },
+  },
 })

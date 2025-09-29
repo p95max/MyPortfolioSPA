5dcee7ed1b5b09efd81119a1a200203945da76ec
maxx_PC <m.petrykin@gmx.de>
2025-09-29 22:18:22 +0200

    logs: workers configured

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,8 +1,20 @@
 import multiprocessing
 import os
 
-workers = multiprocessing.cpu_count() * 2 + 1
-threads = 2
+memory_limit_mb = int(os.getenv("MEMORY_LIMIT", "512"))
+
+if memory_limit_mb <= 512:
+    workers = 1
+    threads = 2
+elif memory_limit_mb <= 1024:
+    workers = 2
+    threads = 2
+else:
+    workers = min(4, multiprocessing.cpu_count())
+    threads = 2
+
 bind = "0.0.0.0:" + os.getenv("PORT", "8000")
 loglevel = "info"
 graceful_timeout = 30
+max_requests = 1000
+max_requests_jitter = 100

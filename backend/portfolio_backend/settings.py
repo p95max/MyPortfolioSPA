14de64aae7e4b2f0171c48aa8f42d9a277443b87
maxx_PC <m.petrykin@gmx.de>
2025-09-29 22:01:45 +0200

    logs: sentry new viti log added

--- backend/portfolio_backend/settings.py
+++ backend/portfolio_backend/settings.py
@@ -75,6 +75,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    "utils.new_visit.LogVisitMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
---
+++ backend/utils/new_visit.py
@@ -0,0 +1,18 @@
+import sentry_sdk
+
+class LogVisitMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        if response.status_code == 200:
+            if request.path == "/":
+                sentry_sdk.capture_message(f"👤 User opened homepage from {request.META.get('REMOTE_ADDR')}")
+            elif request.path.startswith("/admin/"):
+                sentry_sdk.capture_message(f"🔐 Admin access from {request.META.get('REMOTE_ADDR')}")
+            elif request.path == "/api/projects/":
+                sentry_sdk.capture_message(f"📊 API projects accessed from {request.META.get('REMOTE_ADDR')}")
+
+        return response

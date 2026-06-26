f593f898feb5ab6154644ab2912262be37376fc4
maxx-robotron-pc <p95maxde@gmail.com>
2026-06-26 11:45:00 +0200

    sentry remove

--- backend/portfolio_backend/settings.py
+++ backend/portfolio_backend/settings.py
@@ -108,8 +108,6 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 
-    "utils.new_visit.LogVisitMiddleware",
-
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
